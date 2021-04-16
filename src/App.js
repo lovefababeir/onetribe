88abc9d4df9859d509a17adcce594bc2a51091ce
Love Fababeir <love.fababeir@hotmail.com>
2021-04-15 20:25:30 -0400

    removed layout component which is already in each page's component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import "./App.scss";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Splash from "./pages/spash.js";
 import Donate from "./pages/donate.js";
-import Layout from "./components/layout";
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Layout>
-					<Switch>
-						<Route path="/" exact component={Splash} />
-						<Route path="/donate" component={Donate} />
-					</Switch>
-				</Layout>
+				<Switch>
+					<Route path="/" exact component={Splash} />
+					<Route path="/donate" component={Donate} />
+				</Switch>
 			</BrowserRouter>
 		</div>
 	);
