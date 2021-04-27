378910a7e6632e7047776237837e7582d1694859
Love Fababeir <love.fababeir@hotmail.com>
2021-04-26 23:36:19 -0400

    set function of options to update state of donation info

--- src/components/donationFormPaymentFrequency.js
+++ src/components/donationFormPaymentFrequency.js
@@ -1,29 +1,33 @@
-import React from "react"
+import React from "react";
 
-const DonationFormPaymentFrequency = () => {
-  return (
-    <div>
-      <input
-        type="radio"
-        className="paymentInfo__frequency"
-        name="frequency"
-        value="monthly"
-        id="monthly"
-      ></input>
-      <label htmlFor="monthly" className="label__frequency-label form-label">
-        Monthly
-      </label>
-      <input
-        type="radio"
-        className="paymentInfo__frequency"
-        name="frequency"
-        value="once"
-        id="once"
-      ></input>
-      <label htmlFor="once" className="label__frequency-label form-label">
-        One Time
-      </label>
-    </div>
-  )
-}
-export default DonationFormPaymentFrequency
+const DonationFormPaymentFrequency = ({ setDonationInfo, donationInfo }) => {
+	return (
+		<div>
+			<input
+				type="radio"
+				className="paymentInfo__frequency"
+				name="frequency"
+				value="monthly"
+				id="monthly"
+				checked={donationInfo.frequency === "monthly"}
+				onChange={() => setDonationInfo({ ...donationInfo, frequency: "monthly" })}
+			></input>
+			<label htmlFor="monthly" className="label__frequency-label form-label">
+				Monthly
+			</label>
+			<input
+				type="radio"
+				className="paymentInfo__frequency"
+				name="frequency"
+				value="once"
+				id="once"
+				checked={donationInfo.frequency === "once"}
+				onChange={() => setDonationInfo({ ...donationInfo, frequency: "once" })}
+			></input>
+			<label htmlFor="once" className="label__frequency-label form-label">
+				One Time
+			</label>
+		</div>
+	);
+};
+export default DonationFormPaymentFrequency;
